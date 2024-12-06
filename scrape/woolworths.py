a7add94b0f333c44e2af2e5577551557aa8e7773
gusmorris1998 <gusmorris1998@gmail.com>
2024-12-07 08:55:29 +1100

    Finished Woolworths scrape, need to run and test

--- scrape/woolworths.py
+++ scrape/woolworths.py
@@ -2,7 +2,7 @@
 from typing import Literal
 import os, time, json, random
 
-from pydantic import BaseModel, validator
+from pydantic import BaseModel, field_validator
 from curl_cffi import requests
 from dotenv import load_dotenv
 
@@ -10,16 +10,16 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:132.0) Gecko/20100101 Firefox/132.0'
 }
 
-CATEGORIES = {
-    '1-E5BEE36E_SPECIALS': ('/shop/browse/fruit-veg/fruit-veg-specials', '%7B%22name%22:%22Fruit%20%26%20Veg%20Specials%22%7D')
-}
+CATEGORIES = [
+    ('1-E5BEE36E_SPECIALS', '/shop/browse/fruit-veg/fruit-veg-specials', '%7B%22name%22:%22Fruit%20%26%20Veg%20Specials%22%7D')
+]
 
 load_dotenv()
 
 # --- Model Data ---
 
 class AdditionalInformation(BaseModel):
-    nutritionalinformation: Json
+    nutritionalinformation: dict
     sapcategoryname: str
     PiesProductDepartmentsjson: str
 
@@ -51,10 +51,14 @@ class SearchResponse(BaseModel):
     Bundles: list[Product]
 
 def new_session():
-    session = requests.Session(impersonate="chrome", proxy=os.getenv("PROXY"), headers=HEADERS, verify=False)
+    session = requests.Session(impersonate="chrome", proxy=os.getenv("PROXY_PORT_DC"), headers=HEADERS, verify=False)
     return session
 
-def search_api(session: requests.Session, page_num: int) -> SearchResponse:
+def search_api(session: requests.Session,
+                catID: str,
+                catURL: str,
+                catOBJ: str,
+                page_num: int) -> SearchResponse:
 
     url = f"https://www.woolworths.com.au/apis/ui/browse/category?
             categoryId={catID}&
@@ -74,6 +78,26 @@ def search_api(session: requests.Session, page_num: int) -> SearchResponse:
 
     resp.raise_for_status()
 
-    search = SearchResponse(**resp.json()["pageProps"]["searchResults"])
+    search = SearchResponse(**resp.json())
     return search
 
+def main():
+    products = []
+
+    page_num=1
+    session = new_session()
+    
+    for catID, catURL, catOBJ in CATEGORIES:
+        page_end = False
+        while not page_end:
+            page_num += 1
+            time.sleep(random.randint(1,3))
+
+            search = search_api(session, catID, catURL, catOBJ)
+            print(search.Bundles[0].Name if search.Bundles else "Category End")
+            
+            products = products + [p.json for p in search.Bundles]
+
+    # This hasn't worked previously, likely needs changing
+    with open("../storage/data.json", "w") as f:
+        json.dump(products, f, indent=4)
