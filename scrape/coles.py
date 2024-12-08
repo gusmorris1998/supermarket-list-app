f2c5aa04c925f14520d0006b9fbe59d7ea366a74
gusmorris1998 <gusmorris1998@gmail.com>
2024-12-08 19:37:10 +1100

    All scraping code completed for time being (Woolworths, Coles\)

--- scrape/coles.py
+++ scrape/coles.py
@@ -1,15 +1,10 @@
 # Standard Libraries
-from typing import Literal
+from typing import Literal, List, Any, Optional
 import os, time, json, random
 
 from pydantic import BaseModel, field_validator
 from curl_cffi import requests
 from dotenv import load_dotenv
-
-# PROXIES = {
-#     'http': '127.0.0.1:24000',
-#     'https': '127.0.0.1:24000'
-# }
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:132.0) Gecko/20100101 Firefox/132.0'
@@ -17,16 +12,94 @@ HEADERS = {
 
 load_dotenv()
 
+class Category(BaseModel):
+    id: str
+    level: int
+    name: str
+    originalName: str
+    catalogGroupView: List["Category"] = []
+    productCount: int
+
+    class Config:
+        # Allow recursive types (e.g., Children containing more Categories)
+        arbitrary_types_allowed = True
+
+def get_categories():
+    json_data = {
+    'query': '\n    query GetProductCategories($storeId: BrandedId!, $withCampaignLinks: Boolean!, $campaignCount: Int) {\n  productCategories(\n    storeId: $storeId\n    withCampaignLinks: $withCampaignLinks\n    campaignCount: $campaignCount\n  ) {\n    ...productCategoriesFields\n  }\n}\n    \n    fragment productCategoriesFields on ProductCategories {\n  excludedCategoryIds\n  catalogGroupView {\n    ...catalogGroupFields\n    catalogGroupView {\n      ...catalogGroupFields\n      catalogGroupView {\n        ...catalogGroupFields\n      }\n    }\n  }\n}\n    \n    fragment catalogGroupFields on ProductCategory {\n  id\n  level\n  name\n  originalName\n  productCount\n  seoToken\n  type\n}\n    ',
+    'variables': {
+        'storeId': 'COL:512',
+        'withCampaignLinks': False,
+    },
+    'operationName': 'GetProductCategories',
+}
+    cookies = {
+        'visid_incap_2800108': 'xCHbqHKvTs6XYjJu2ki6D7U4VGcAAAAAQUIPAAAAAADFQulN3t02f+q2uiOygfxC',
+        'incap_ses_361_2800108': 's6Xvd9BWgUZqHDJHx4cCBbU4VGcAAAAAH0+28AHrFtCoWF91fnJ+UA==',
+        'AMCVS_0B3D037254C7DE490A4C98A6%40AdobeOrg': '1',
+        'AMCV_0B3D037254C7DE490A4C98A6%40AdobeOrg': '179643557%7CMCIDTS%7C20065%7CMCMID%7C11519788548200838052708678877733750465%7CMCAID%7CNONE%7CMCOPTOUT-1733579991s%7CNONE%7CvVersion%7C5.5.0',
+        'nlbi_2800108_2147483392': '4xbpfg2BLwtUEHHUjQyMFgAAAACDdUQhnRORD7+Odf/W0WRn',
+        'reese84': '3:4nXhj8drlacAuckrgXtqHA==:ggEw/rViZTny6JMt67o6ihaDGeq/FhIuNRi044dMIG6LPtYoPLTOrebt07Z72VHf1PwwXi8Isgi0/Y41WVKMvJ56WmYq6cqD8OCz1fsdNDZD5lA24uNjOKlPNQyEsEXXqiMoYljtTEp8DzSHilv41Vxtlcxv4oFcpeUuJiDM0UgSSh1bWCFykq+9Pjok1zBA0tnG/Ov6BtcV6TzR+oxNtpk+T8IoGGWQXFM60BCk8hMe8v3mrII3DnY4nrXl+exdcCbVeISH/myYcMJ9JOSAgdzr5EDDPNZajnn7hbl4F/eftucGd7Y3UBxbgmNdeoCjPhPKHfwswIklPV4eYVsfgBzPuQQtnbwX/IJjauKplIOXNPjMeosU2yVvQDwb///DUuhKy5MvkyKe6ut6s8XrDU626VyqTlApVcCYuFMGrlVo19KFordEucmQpkvc7SoZXuKQ2WZjTsmQ2uSPMsD5Sw==:exZeCT8KdU2fvUd0fp1O8mY1qkA9TR6nbij3ku5sFUk=',
+        'ld_user': 'c901501c-be39-4fdc-a426-8aaab5cdc444',
+        'sessionId': '8a83b5c0-efe3-4376-a968-28032fa25f7f',
+        'visitorId': '9ee79307-bda0-4b2a-85fe-cf0de56311fd',
+        'nlbi_2800108_3037207': '5P5PX79tf1s8QDwqjQyMFgAAAAAVzzwEhA7ZWsrQVNTPTP+m',
+        'analyticsIsLoggedIn': 'false',
+        'at_check': 'true',
+        'mbox': 'session#70822cdd1da14a5bb45ed07985ac0360#1733574657|PC#70822cdd1da14a5bb45ed07985ac0360.36_0#1796817597',
+        'fulfillmentStoreId': '0512',
+        'shopping-method': 'clickAndCollect',
+    }
+
+    headers = {
+        'accept': '*/*',
+        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
+        'content-type': 'application/json',
+        # 'cookie': 'visid_incap_2800108=xCHbqHKvTs6XYjJu2ki6D7U4VGcAAAAAQUIPAAAAAADFQulN3t02f+q2uiOygfxC; incap_ses_361_2800108=s6Xvd9BWgUZqHDJHx4cCBbU4VGcAAAAAH0+28AHrFtCoWF91fnJ+UA==; AMCVS_0B3D037254C7DE490A4C98A6%40AdobeOrg=1; AMCV_0B3D037254C7DE490A4C98A6%40AdobeOrg=179643557%7CMCIDTS%7C20065%7CMCMID%7C11519788548200838052708678877733750465%7CMCAID%7CNONE%7CMCOPTOUT-1733579991s%7CNONE%7CvVersion%7C5.5.0; nlbi_2800108_2147483392=4xbpfg2BLwtUEHHUjQyMFgAAAACDdUQhnRORD7+Odf/W0WRn; reese84=3:4nXhj8drlacAuckrgXtqHA==:ggEw/rViZTny6JMt67o6ihaDGeq/FhIuNRi044dMIG6LPtYoPLTOrebt07Z72VHf1PwwXi8Isgi0/Y41WVKMvJ56WmYq6cqD8OCz1fsdNDZD5lA24uNjOKlPNQyEsEXXqiMoYljtTEp8DzSHilv41Vxtlcxv4oFcpeUuJiDM0UgSSh1bWCFykq+9Pjok1zBA0tnG/Ov6BtcV6TzR+oxNtpk+T8IoGGWQXFM60BCk8hMe8v3mrII3DnY4nrXl+exdcCbVeISH/myYcMJ9JOSAgdzr5EDDPNZajnn7hbl4F/eftucGd7Y3UBxbgmNdeoCjPhPKHfwswIklPV4eYVsfgBzPuQQtnbwX/IJjauKplIOXNPjMeosU2yVvQDwb///DUuhKy5MvkyKe6ut6s8XrDU626VyqTlApVcCYuFMGrlVo19KFordEucmQpkvc7SoZXuKQ2WZjTsmQ2uSPMsD5Sw==:exZeCT8KdU2fvUd0fp1O8mY1qkA9TR6nbij3ku5sFUk=; ld_user=c901501c-be39-4fdc-a426-8aaab5cdc444; sessionId=8a83b5c0-efe3-4376-a968-28032fa25f7f; visitorId=9ee79307-bda0-4b2a-85fe-cf0de56311fd; nlbi_2800108_3037207=5P5PX79tf1s8QDwqjQyMFgAAAAAVzzwEhA7ZWsrQVNTPTP+m; analyticsIsLoggedIn=false; at_check=true; mbox=session#70822cdd1da14a5bb45ed07985ac0360#1733574657|PC#70822cdd1da14a5bb45ed07985ac0360.36_0#1796817597; fulfillmentStoreId=0512; shopping-method=clickAndCollect',
+        'cusp-correlation-id': 'e4263d7c-4d36-4492-a897-5a6b0cbcf62d',
+        'cusp-session-id': '8a83b5c0-efe3-4376-a968-28032fa25f7f',
+        'cusp-user-id': '',
+        'cusp-visitor-id': '9ee79307-bda0-4b2a-85fe-cf0de56311fd',
+        'dsch-channel': 'coles.online.1site.desktop',
+        'ocp-apim-subscription-key': 'eae83861d1cd4de6bb9cd8a2cd6f041e',
+        'origin': 'https://www.coles.com.au',
+        'priority': 'u=1, i',
+        'referer': 'https://www.coles.com.au/',
+        'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+        'sec-fetch-dest': 'empty',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-site': 'same-origin',
+        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36',
+    }
+
+    response = requests.post(f'{os.getenv('COLES_API')}/graphql',
+                            headers=headers,
+                            cookies=cookies,
+                            json=json_data)
+                            # proxy=os.getenvb('PROXY_PORT_DC'))
+
+    categories = [
+        Category(**cat).model_dump_json() for cat in response.json()['data']['productCategories']['catalogGroupView']
+    ]
+
+    with open(f"/{os.getenv("WDIR")}/scrape/storage/coles_cats.json", "w") as f:
+        json.dump(categories, f, indent=4)
+
+
 class Product(BaseModel):
     # General Details
     _type: Literal["PRODUCT"]
     id: int
     name: str
-    size = str
+    brand: str | None
+    size: str
 
     # Dicts
     pricing: dict
     merchandiseHeir: dict
+    onlineHeirs: Optional[List[dict]] = None  # Allow missing `onlineHeirs`
 
 class SearchResponse(BaseModel):
     noOfResults: int
@@ -34,33 +107,33 @@ class SearchResponse(BaseModel):
     start: int
     results: list[Product | None]
 
-    @field_validator('results', pre=True, each_item=True)
-    def filter_non_product(cls, v):
-        if v.get("_type") == "PRODUCT" and v.get("pricing") is not None:
-            return v  # Keep valid entries
-        return None  # Discard invalid entries
-    
-    def __init__(self, **data):
-        super().__init__(**data)
-        # Filter out None values from results
-        self.results = [result for result in self.results if result is not None]
+    @field_validator("results", mode="before")
+    def filter_non_product(cls, results: Any) -> Any:
+        # Ensure only valid Product objects with "_type" == "PRODUCT" and "pricing" are kept
+        filtered_results = []
+        for item in results:
+            if not isinstance(item, dict):  # Discard non-dict entries early
+                continue
+            if item.get("_type") == "PRODUCT" and item.get("pricing") is not None:
+                filtered_results.append(item)
+        return filtered_results
 
 def new_session():
-    session = requests.Session(impersonate="chrome", proxy=os.getenv("PROXY"), headers=HEADERS, verify=False)
+    session = requests.Session(impersonate="chrome",
+                                proxy=os.getenv("PROXY"),
+                                headers=HEADERS,
+                                verify=False)
     return session
 
 def search_api(session: requests.Session, page_num: int) -> SearchResponse: 
-    url = f"https://www.coles.com.au/_next/data/20241130.04_v4.33.2/en/on-special.json?page={page_num}"
+    url = f"{os.getenv("COLES_DATA_API")}/en/on-special.json?page={page_num}"
 
     resp = session.get(url)
-
-    i = 1
-    while i <= 3 and not resp.ok:
-        try:
-            resp.raise_for_status()
-        except:
-            time.sleep(i*2)
-            resp = session.get(url)
+    for i in range(1, 4):
+        if resp.ok:
+            break
+        time.sleep(i * 2)
+        resp = session.get(url)
 
     resp.raise_for_status()
 
@@ -73,7 +146,7 @@ def main():
     session = new_session()
     
     search = search_api(session, page_num)
-    products = [p.json() for p in search.results]
+    products = [p.model_dump_json() for p in search.results]
 
     no_pages = search.noOfResults // (search.pageSize + 1)
 
@@ -84,10 +157,10 @@ def main():
         search = search_api(session, page_num)
         print(search.start)
 
-        products = products + [p.json() for p in search.results]
+        products = products + [p.model_dump_json() for p in search.results]
 
-    with open("../storage/data.json", "w") as f:
-        json.dump(products, f, indent=4)
+        with open(f"/{os.getenv("WDIR")}/scrape/storage/coles_products.json", "w") as f:
+            json.dump(products, f, indent=4) 
 
 if __name__ ==  "__main__":
     main()
--- scrape/woolworths.py
+++ scrape/woolworths.py
@@ -1,6 +1,7 @@
 # Standard Libraries
-from typing import Literal
-import os, time, json, random
+from typing import List, Union
+from typing_extensions import Annotated
+import os, time, json, random, re
 
 from pydantic import BaseModel, field_validator
 from curl_cffi import requests
@@ -10,25 +11,43 @@ HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:132.0) Gecko/20100101 Firefox/132.0'
 }
 
-CATEGORIES = [
-    ('1-E5BEE36E_SPECIALS', '/shop/browse/fruit-veg/fruit-veg-specials', '%7B%22name%22:%22Fruit%20%26%20Veg%20Specials%22%7D')
-]
-
 load_dotenv()
 
-# --- Model Data ---
+class Category(BaseModel):
+    NodeId: str
+    Description: str
+    UrlFriendlyName: str
+    Children: List["Category"] = []
+
+    class Config:
+        # Allow recursive types (e.g., Children containing more Categories)
+        arbitrary_types_allowed = True
+
+def get_categories():
+    url = f"{os.getenv("WOOLWORTHS_API")}/PiesCategoriesWithSpecials"
+
+    resp = requests.get(url, proxy=os.getenv("PROXY_PORT_DC"))
+    resp.raise_for_status()
+
+    categories = [
+        Category(**cat).model_dump_json() for cat in resp.json()["Categories"]
+    ]
+
+    with open(f"/{os.getenv("WDIR")}/scrape/storage/coles_cats.json", "w") as f:
+        json.dump(categories, f, indent=4)
+
 
 class AdditionalInformation(BaseModel):
-    nutritionalinformation: dict
-    sapcategoryname: str
+    nutritionalinformation: str | None
+    sapcategoryname: str | None
     PiesProductDepartmentsjson: str
 
 class ProductDetails(BaseModel):
     # General Details
     Stockcode: int
-    Price: float
+    Price: float | None
     WasPrice: float
-    CupPrice: int | None
+    CupPrice: float | None
     CupMeasure: str | None
     IsNew: bool
     IsAvailable: bool
@@ -40,15 +59,35 @@ class ProductDetails(BaseModel):
     MediumImageFile: str
     AdditionalAttributes: AdditionalInformation
 
+
 class Product(BaseModel):
-    Products: list[ProductDetails]
     Name: str
     DisplayName: str
+
+    @field_validator('Products', mode='before')
+    @classmethod
+    def validate_Products(cls, products):
+        return [
+            product for product in products
+            if isinstance(product.get('Price'), (int, float))
+        ]
+
+    Products: List[ProductDetails]
 
 
 class SearchResponse(BaseModel):
     TotalRecordCount: int
+
+    @field_validator("Bundles", mode="before")
+    def filter_invalid_bundles(cls, bundles):
+        # Filter out Products with no valid ProductDetails
+        return [
+            bundle for bundle in bundles 
+            if len(bundle.get("Products", [])) > 0
+        ]
+
     Bundles: list[Product]
+
 
 def new_session():
     session = requests.Session(impersonate="chrome", proxy=os.getenv("PROXY_PORT_DC"), headers=HEADERS, verify=False)
@@ -60,11 +99,7 @@ def search_api(session: requests.Session,
                 catOBJ: str,
                 page_num: int) -> SearchResponse:
 
-    url = f"https://www.woolworths.com.au/apis/ui/browse/category?
-            categoryId={catID}&
-            Url={catURL}&
-            formatObject={catOBJ}&
-            pageNumber={page_num}"
+    url = f"{os.getenv("WOOLWORTHS_API")}/browse/category?categoryId={catID}&Url={catURL}&formatObject={catOBJ}&pageNumber={page_num}&pageSize=36"
 
     resp = session.get(url)
 
@@ -81,23 +116,42 @@ def search_api(session: requests.Session,
     search = SearchResponse(**resp.json())
     return search
 
+def get_details(node, url):
+    if re.search('SPECIALS', node['NodeId']):
+        return (node['NodeId'],
+                f"{url}/{node["UrlFriendlyName"]}",
+                f'%7B"name":"{re.sub('&', '%26', node['Description'])}"%7D')
+    
+    return get_details(node["Children"][0], f"{url}/{node["UrlFriendlyName"]}")
+
 def main():
     products = []
-
-    page_num=1
+    with open(f"/{os.getenv("WDIR")}/scrape/storage/coles_cats.json") as f:
+        json_data = json.load(f)[1:21]
+        categories = [
+            get_details(json.loads(node), '/shop/browse') for node in json_data
+                        ]
     session = new_session()
     
-    for catID, catURL, catOBJ in CATEGORIES:
-        page_end = False
-        while not page_end:
-            page_num += 1
+    for catID, catURL, catOBJ in categories:
+        page_num = 1
+        while True:
             time.sleep(random.randint(1,3))
 
-            search = search_api(session, catID, catURL, catOBJ)
-            print(search.Bundles[0].Name if search.Bundles else "Category End")
-            
-            products = products + [p.json for p in search.Bundles]
+            search = search_api(session, catID, catURL, catOBJ, page_num)
 
-    # This hasn't worked previously, likely needs changing
-    with open("../storage/data.json", "w") as f:
+            if search.Bundles:
+                print(search.Bundles[0].Name)
+            else:
+                print("Category End")
+                break
+            
+            products = products + [p.model_dump_json()  for p in search.Bundles]
+
+            page_num += 1
+
+    with open(f"/{os.getenv("WDIR")}/scrape/storage/coles_products.json", "w") as f:
         json.dump(products, f, indent=4)
+
+if __name__ ==  "__main__":
+    main()
